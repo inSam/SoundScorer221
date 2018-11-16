0511467af0911114942630a26ea4ac432675cb24
Eric Tang <erictang1729@gmail.com>
2018-11-16 09:20:24 -0800

    Pull 7473 songs from top 1000 albums. Add to writeup

---
+++ write_tracks.py
@@ -0,0 +1,23 @@
+"""
+Takes in tracks from util.py, writes them to json file for future use
+"""
+
+import json
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+import util
+
+DATA_PATH = "data/tracks.json"
+
+if __name__ == "__main__":
+    CLIENT_ID="810e2eec841546269c49f338f1be189a"
+    CLIENT_SECRET="764290d0ab0f4f4484b0d4c7b701bdee"
+    token = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    cache_token = token.get_access_token()
+    sp = spotipy.Spotify(cache_token)
+
+    tracks = util.tracks_from_albums(sp, 2018, 1000)
+    with open(DATA_PATH,'w') as ff:
+        json.dump(tracks, ff )
